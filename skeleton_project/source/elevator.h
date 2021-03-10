004286eb4beffdfe3546915a408b0a6f6b42dc64
sunnivlf <sunnivlf@stud.ntnu.no>
2021-03-10 09:53:24 +0100

    Update elevator.h

--- skeleton_project/source/elevator.h
+++ skeleton_project/source/elevator.h
@@ -31,15 +31,15 @@ typedef struct elevator{
 } elevator;
 
 /**
- * @brief Initializes the elevator. Moves to defined state.
+ * @brief Initializes the elevator and moves it to a defined state.
  * @param[in, out] el The elevator.
  */
 void elev_init(elevator* el);
 
 /**
- * @brief Gets the current floor. 
+ * @brief Gets the floor the elevator is currently on.
  * 
- * @return -1 if the elevator is between floors, otherwise the floor index.
+ * @return -1 if the elevator is between floors, otherwise the floor's index.
  */
 int elev_get_current_floor();
 
@@ -52,19 +52,20 @@ int elev_get_current_floor();
 void elev_control_range(elevator* el);
 
 /**
- * @brief Sets the motor direction. Depending on previous
+ * @brief Sets the motor direction depending on previous
  * direction and next orders.
  * 
- * @param[in, out] el The elevator.
+ * @param[in] el The elevator.
  * 
  * @return @c HARDWARE_MOVEMENT_UP, @c HARDWARE_MOVEMENT_STOP, @c HARDWARE_MOVEMENT_DOWN
  */
 HardwareMovement elev_set_motor_dir(elevator* el);
 
 /**
- * @brief Sets the floor indicator light for the current floor.
- * If the elevator is between to floors, the floor indicator light
- * for the floor the elevator just left is lit.
+ * @brief Sets the floor indicator light for the floor the elevator
+ * is currently on. If the elevator is between two floors, the floor 
+ * indicator light is lit for the floor the elevator just left.
+ * 
  * @param[in] el The elevator.
  */
 void elev_set_floor_indicator(elevator* el);
@@ -73,12 +74,15 @@ void elev_set_floor_indicator(elevator* el);
  * @brief Updates the directions. The previous direction is set
  * equal to the current direction and the current direction is set
  * by @c elev_set_motor_dir.
- * @param[in] el The elevator.
+ * 
+ * @param[in, out] el The elevator.
  */
 void elev_update_dir(elevator* el);
 
 /**
- * @brief Sets the current floor.
+ * @brief Sets the current floor when the elevator reaches a new
+ * floor.
+ * 
  * @param[in, out] el The elevator.
  */
 void elev_set_current_floor(elevator* el);
