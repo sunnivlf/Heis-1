6740982847f62e9efce61dc9deb167cae3089e5f
sunnivlf <sunnivlf@stud.ntnu.no>
2021-03-10 10:07:18 +0100

    Update timer.h

--- skeleton_project/source/timer.h
+++ skeleton_project/source/timer.h
@@ -1,16 +1,14 @@
-
-#include <time.h>
-#include <stdio.h>
-#include "elevator.h"
-
 /**
  * @file
  * @brief Functions to keep the door open for a period of time.
  */
 
+#ifndef TIMER_H
+#define TIMER_H
+#include <time.h>
+#include <stdio.h>
+#include "elevator.h"
 #define DOOR_TIME 3
-
-
 
 /**
  * @brief Starts the timer. 
@@ -18,12 +16,13 @@
  */
 void start_timer(elevator* el);
 
-
 /**
  * @brief Checks if the time is up. 
  * @param[in, out] el The elevator.
  * @return 1 if the time is up, 0 if not. 
  */
 int times_up(elevator* el);
+
+#endif // #ifndef TIMER_H
 
 
